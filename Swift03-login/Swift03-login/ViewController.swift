75286a703e8c381a8d9db8a785b98178c4b99a10
folsi <jerfolsi@gmail.com>
2016-08-10 23:10:51 +0200

    add tutos

---
+++ Swift03-login/Swift03-login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift03-login
+//
+//  Created by jerome on 10/08/2016.
+//  Copyright © 2016 fr.successclub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift04-autolayout/README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=nNZfC20p1T4
